45cae4a4f50806d6888ebc37416317c933907225
Cvetelina5566 <tsvete.pete@gmail.com>
2025-06-17 11:52:35 +0300

    I am trying to add some tests

---
+++ PenRealm.Tests/PoemTests.cs
@@ -0,0 +1,34 @@
+﻿using NuGet.ContentModel;
+using PenRealm.Models;
+using Xunit;
+
+namespace PenRealm.Tests
+{
+    public class PoemTests
+    {
+        [Fact]
+        public void Poem_Without_Title_Should_Be_Invalid()
+        {
+            var poem = new Poem
+            {
+                Title = null,
+                Content = "Some text"
+            };
+
+            Assert.Null(poem.Title);
+        }
+
+        [Fact]
+        public void Poem_With_Title_Should_Be_Valid()
+        {
+            var poem = new Poem
+            {
+                Title = "My Title",
+                Content = "Some text"
+            };
+
+            Assert.NotNull(poem.Title);
+        }
+    }
+}
+
